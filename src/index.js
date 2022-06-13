6e1e070f5e13bb5044b3f337fe1b3f304cf4b2e4
aliceauko <alice.auko@student.moringaschool.com>
2022-06-13 14:40:36 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
+//document.addEventListener("DOMContentLoaded", () => {
   // your code here
+//});
+document.addEventListener("DOMContentLoaded", () => {
+
+  const taskForm = document.getElementById('create-task-form');
+  const dataInput = document.getElementById('new-task-description')
+  const tasksUl = document.getElementById('tasks')
+
+    taskForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      workApp()
+    })
+
+    const newTask = () => {
+      tasksUl.innerHTML = `<li>${dataInput.value}</li>`
+    }
+
+    const workApp = () => {
+      if(tasksUl.innerHTML) {
+        newTask()
+      } else {
+          console.log('fail')
+      }
+      
+    }    
 });
+
