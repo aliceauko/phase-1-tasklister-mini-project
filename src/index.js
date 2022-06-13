e95e50cd68d21aed33150aee11fbceaa0cc0193f
aliceauko <alice.auko@student.moringaschool.com>
2022-06-13 14:42:21 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-//document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-//});
 document.addEventListener("DOMContentLoaded", () => {
 
   const taskForm = document.getElementById('create-task-form');
